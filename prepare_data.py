9d53c850e02828fa2f7610f657a62021a89da5bd
geniusfoever <58506698+geniusfoever@users.noreply.github.com>
2022-12-13 19:01:30 +0800

    yes

--- prepare_data.py
+++ prepare_data.py
@@ -5,13 +5,15 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='for face verification')
-    parser.add_argument("-r", "--rec_path", help="mxnet record file path",default='glint', type=str)
+    parser.add_argument("-r", "--rec_path", help="mxnet record file path",default=r"D:\DataBase\51\lfw_output\masked_pair", type=str)
     args = parser.parse_args()
     conf = get_config()
-    rec_path = conf.data_path/args.rec_path
+    rec_path = args.rec_path
     # load_mx_rec(rec_path)
 
-    bin_files = ['lfw']#'agedb_30', 'cfp_fp', ] #, 'calfw', 'cfp_ff', 'cplfw', 'vgg2_fp']
-    
-    for i in range(len(bin_files)):
-        load_bin(rec_path/(bin_files[i]+'.bin'), rec_path/bin_files[i], conf.test_transform)
+    # bin_files = ['lfw']#'agedb_30', 'cfp_fp', ] #, 'calfw', 'cfp_ff', 'cplfw', 'vgg2_fp']
+    #
+    # for i in range(len(bin_files)):
+    #     load_bin(rec_path/(bin_files[i]+'.bin'), rec_path/bin_files[i], conf.test_transform)
+
+    load_bin(Path(rec_path+'.bin'), Path(rec_path), conf.test_transform)
